a4ec601046fc5c94e2211b79c33993e3723ddd8b
Habib-bhai <habibahmed918131@gmail.com>
2024-10-12 00:30:53 +0500

    add hello and console logs

--- test.ts
+++ test.ts
@@ -1,1 +1,7 @@
 console.log("hello world")
+console.log("hello ")
+console.log("hello world2")
+console.log("hello2 ")
+console.log("hello world 3")
+console.log("hello 3")
+console.log("hello world 4")
